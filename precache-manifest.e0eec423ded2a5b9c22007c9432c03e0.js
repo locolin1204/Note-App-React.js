a54233fb9bc998c357b96ee5af33f92ae904dc6b
locolin <locolin99@gmail.com>
2022-05-30 22:33:49 +0800

    Updates

---
+++ precache-manifest.e0eec423ded2a5b9c22007c9432c03e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a99ba8020c0e7be04799",
+    "url": "/React.js-Note-App/static/css/main.e7f83b9c.chunk.css"
+  },
+  {
+    "revision": "a99ba8020c0e7be04799",
+    "url": "/React.js-Note-App/static/js/main.a99ba802.chunk.js"
+  },
+  {
+    "revision": "aaf8adbfce1c3265f549",
+    "url": "/React.js-Note-App/static/js/1.aaf8adbf.chunk.js"
+  },
+  {
+    "revision": "d3caa3969221e05bfc10",
+    "url": "/React.js-Note-App/static/js/runtime~main.d3caa396.js"
+  },
+  {
+    "revision": "3140fe888b1e1a04062aac256fb97103",
+    "url": "/React.js-Note-App/index.html"
+  }
+];
