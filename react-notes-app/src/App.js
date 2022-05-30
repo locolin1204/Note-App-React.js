a211ac0b376739ac9e8311d7181d5a71f90c9c9b
locolin <locolin99@gmail.com>
2022-05-30 23:43:44 +0800

    third commit

--- react-notes-app/src/App.js
+++ react-notes-app/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid'
 import NotesList from './components/NotesList';
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+    id: nanoid(),
+    text: "This is my first note!",
+    date: "15/04/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "15/04/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "29/04/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my final note!",
+      date: "30/04/2022",
+    },
+      
+
+]);
   return (<div className="container">
-    <NotesList />
+    <NotesList notes={notes} />
   </div>)
 };
 
--- react-notes-app/src/components/Note.js
+++ react-notes-app/src/components/Note.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {MdDeleteForever} from "react-icons/md";
 
 
-const Note = () => {
+const Note = ({ id, text, date }) => {
     return(
         <div className="note">
-            <span>Hello! This is our first note! Hurry</span>
+            <span>{text}</span>
             <div className="note-footer">
-                <small>13/04/2022</small>
+                <small>{date}</small>
                 <MdDeleteForever className = "delete-icon" size = "1.3em"/>
             </div>
         </div>
