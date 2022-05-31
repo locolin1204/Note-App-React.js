a525497575ccb8867b295be566f7e183c7f3ba60
locolin <locolin99@gmail.com>
2022-05-31 17:00:35 +0800

    added dark mode

--- react-notes-app/src/App.js
+++ react-notes-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { nanoid } from 'nanoid'
 import NotesList from './components/NotesList';
 import Search from './components/Search';
+import Header from './components/Header';
 
 const App = () => {
   const [notes, setNotes] = useState([
@@ -26,7 +27,10 @@ const App = () => {
       date: "30/04/2022",
     },
 ]);
+  const[darkMode, setDarkMode] = useState(false);
 
+  const [searchText, setSearchText] = useState("");
+  
   const addNote = (text) => {
     const date = new Date();
     const newNote = {
@@ -38,20 +42,26 @@ const App = () => {
     setNotes(newNotes);
   }
 
+
   const deleteNote = (id) => {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
   }
 
+
   return (
-  <div className="container">
-    <Search />
-    <NotesList 
-      notes={notes}
-      handleAddNote = {addNote}
-      handleDeleteNote = {deleteNote}
-    />
-  </div>)
+    <div className={`${darkMode && "dark-mode"}`}>
+      <div className="container">
+      <Header handleToggleDarkMode = {setDarkMode} />
+      <Search handleSearchNote = {setSearchText} />
+      <NotesList 
+        notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))}
+        handleAddNote = {addNote}
+        handleDeleteNote = {deleteNote}
+      />
+      </div>
+    </div>
+  )
 };
 
 export default App;
--- react-notes-app/src/components/Search.js
+++ react-notes-app/src/components/Search.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { MdSearch } from 'react-icons/md';
 
 
-const Search = () => {
+const Search = ({ handleSearchNote }) => {
     return (
     <div className="search">
         <MdSearch
             className="search-icons"
             size="1.3em"
         />
-        <input type="text" placeholder="Type to search..."/>
+        <input 
+            type="text" 
+            placeholder="Type to search..."
+            onChange={(event) => {
+                handleSearchNote(event.target.value)
+            }} 
+        />
     </div>
     );
 }
